9eaf3faceeca8877b5a9a6bd840995909f0b7550
Oxana Gunchenko <ksuper2007@gmail.com>
2021-07-10 19:52:18 +0300

    Тестирование Credit Card Number Validator main.java README.md CHECK_LIST.md

---
+++ Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+      // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
+      String number = ""; //"5351719427810741";
+      System.out.println(number); //для контроля, катра которая валидировалась
+      System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
+    }
+  
+    public static boolean isValidCardNumber(String number) {
+      if (number == null) {
+        return false;
+      }
+  
+      if (number.length() != 16) {
+        return false;
+      }
+  
+      long result = 0;
+      for (int i = 0; i < number.length(); i++) {
+        int digit;
+        try {
+          digit = Integer.parseInt(number.charAt(i) + "");
+        } catch (NumberFormatException e) {
+          return false;
+        }
+  // Luhn algorythm
+        if (i % 2 == 0) {
+          digit *= 2;
+          if (digit > 9) {
+            digit -= 9;
+          }
+        }
+        result += digit;
+      }
+  
+      return (result != 0) && (result % 10 == 0);
+    }
+  }
